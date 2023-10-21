1566f2a3752aad102ddd84774dcb89071b7831c5
aleks9045 <aleksey9045@gmail.com>
2023-10-21 21:02:30 +0300

    fix

--- app/publication/schemas.py
+++ app/publication/schemas.py
@@ -1,11 +1,10 @@
 from pydantic import BaseModel
-from datetime import datetime
+
 
 class article_schema(BaseModel):
     user_name: str
     title: str
     description: str
-    date: datetime
+    date: str
     theme: str
     file_name: str
-
