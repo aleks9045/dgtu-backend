9e090fd415937e2a96046beb07a041c1827cc199
Shuvi <mskuratik06@gmail.com>
2023-10-22 01:02:08 +0300

    fix test_publication.py

--- tests/test_publication.py
+++ tests/test_publication.py
@@ -6,7 +6,7 @@ from conftest import client, async_session_maker
 
 
 def test_article_add():
-    response = client.post("/articles/add", json=(
+    response = client.post("/articles/add_article", json=(
         {
             "user_name": "string",
             "title": "string",
@@ -20,9 +20,9 @@ def test_article_add():
     assert response.status_code == 200
 
 def test_add_photo():
-    response = client.post("/articles/photo")
+    response = client.post("/articles/add_photo")
     assert response.status_code == 200 or response.status_code == 422
 
 def test_get_photo():
-    response = client.get("/articles/photo")
+    response = client.get("/articles/get_photo")
     assert response.status_code == 200 or response.status_code == 422
